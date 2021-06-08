8e9cde349f5855172e9933015374b6418fdb6869
RYOTA <kobaryo04ysh@icloud.com>
2021-06-08 15:34:04 +0900

    fix: corsの設定を追加

--- server/server.js
+++ server/server.js
@@ -1,14 +1,30 @@
-io.on('connection', (socket) => {
-    console.log('connected:', socket.id);
-  
-    // 切断時
-    socket.on('disconnect', () => {
-      console.log('disconnected:', socket.id);
-    });
-  
-    // ユーザの参加
-    socket.on('send', (message) => {
-      console.log('send:', message);
-      io.emit('send', message);
-    });
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const express = require("express");
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+// const socketIo = require("socket.io");
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+const server = app.listen(3001, function () {
+  console.log("server running on port 3001");
+});
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+// const io = socketIo(server);
+
+io.on("connection", function (socket) {
+  socket.on("SEND_MESSAGE", function (data) {
+    io.emit("MESSAGE", data);
   });
+});
